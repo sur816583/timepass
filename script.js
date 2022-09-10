ca23818ddfb5526a4f2634f1359ba9579fcfdbe3
21Saurabh <92380646+21Saurabh@users.noreply.github.com>
2022-09-10 12:59:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //const api_url = "<heroku_app_url>"
-const api_url = "http://localhost:8080/user"
+const api_url = "https://immortal21.herokuapp.com/user"
 function loadData(records = []) {
 var table_data = "";
 for(let i=0; i<records.length; i++) {
